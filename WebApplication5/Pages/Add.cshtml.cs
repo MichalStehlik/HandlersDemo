e111be048a402f6955be250d9b31e52fe7bb63fd
Michal Stehlík <michal.stehlik@pslib.cz>
2020-03-27 09:26:27 +0100

    Add project files.

---
+++ WebApplication5/Pages/Add.cshtml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WebApplication5
+{
+    public class AddModel : PageModel
+    {
+        [BindProperty(SupportsGet = true)]
+        public string Input { get; set; } = "";
+        public IActionResult OnGet()
+        {
+            Input = Input + "Ahoj";
+            return Page();
+        }
+
+        public IActionResult OnGetNazdar()
+        {
+            Input = Input + "Nazdar";
+            return RedirectToPage("/Index");
+            //return NotFound(); // 404
+            //return Unauthorized(); // 401
+        }
+
+        public void OnPost()
+        {
+            
+        }
+    }
+}
